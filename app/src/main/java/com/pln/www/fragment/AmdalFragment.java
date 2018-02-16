f7cf11489cc182608515792455530cfe9814db65
apriliaaachi <achiaprilia.aa@gmail.com>
2018-02-16 10:54:24 +0700

    lanjut lanjut

--- app/src/main/java/com/pln/www/fragment/AmdalFragment.java
+++ app/src/main/java/com/pln/www/fragment/AmdalFragment.java
@@ -94,7 +94,7 @@ public class AmdalFragment extends Fragment {
                 PekerjaanModel.class,
                 R.layout.list_view,
                 PekerjaanModelViewHolder.class,
-                dbPekerjaan.child("Pekerjaan")
+                dbPekerjaan.child("Pekerjaan").child("jenisPekerjaan").equalTo("AMDAL")
         ) {
             @Override
             protected void populateViewHolder(final PekerjaanModelViewHolder viewHolder, final PekerjaanModel model, int position) {
